eed3efc49b7c6659ba8ca278ce89892c607cf401
joel0zz <joelkelly93@gmail.com>
2019-06-07 23:17:29 +0800

    redis take 5455

--- Blog/views.py
+++ Blog/views.py
@@ -9,6 +9,7 @@ import boto3
 import io
 import os
 from rq import Queue
+from rq.job import Job
 
 blog_app = Blueprint('blog_app', __name__)
 
@@ -40,6 +41,16 @@ q = Queue(connection=conn)
 def redis():
     result = q.enqueue(redis_test)
     return str(result)
+
+
+@blog_app.route('/results/<job_key>', methods=['GET'])
+def results(job_key):
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Nay!", 202
 
 
 @blog_app.route('/blog')
